b80dff88e2414c86dbdbc8fc70094419a7b07945
mrhassan <rakibz.edu@outlook.com>
2018-02-04 17:22:25 +0000

    boxBlurKernel method

--- src/main/scala/scalashop/package.scala
+++ src/main/scala/scalashop/package.scala
@@ -23,6 +23,39 @@ package object scalashop {
     (r << 24) | (g << 16) | (b << 8) | (a << 0)
   }
 
+  /** Computes the blurred RGBA value of a single pixel of the input image. */
+  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
+    // TODO implement using while loops
+    val minX = clamp(x - radius, 0, src.width - 1)
+    val minY = clamp(y - radius, 0, src.height - 1)
+    val maxX = clamp(x + radius, 0, src.width - 1)
+    val maxY = clamp(y + radius, 0, src.height - 1)
+
+    var curX = minX
+    // processed pixel count
+    var proPix = 0
+    var curR = 0
+    var curG = 0
+    var curB = 0
+    var curA = 0
+
+    while (curX <= maxX) {
+      var curY = minY
+      while (curY <= maxY) {
+        val pix = src.apply(curX, curY)
+        curR+= red(pix)
+        curG+= green(pix)
+        curB+= blue(pix)
+        curA+= alpha(pix)
+        proPix += 1
+        curY+=1
+      }
+      curX+=1
+    }
+    // taking average
+    rgba(curR/proPix, curG/proPix, curB/proPix , curA/proPix)
+  }
+
   /** Restricts the integer into the specified range. */
   def clamp(v: Int, min: Int, max: Int): Int = {
     if (v < min) min
@@ -33,23 +66,10 @@ package object scalashop {
   /** Image is a two-dimensional matrix of pixel values. */
   class Img(val width: Int, val height: Int, private val data: Array[RGBA]) {
     def this(w: Int, h: Int) = this(w, h, new Array(w * h))
-    def apply(x: Int, y: Int): RGBA = data(y * width + x)
-    def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
-  }
 
-  /** Computes the blurred RGBA value of a single pixel of the input image. */
-  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
-    // TODO implement using while loops
-    var a = -radius
-    var sum = 0
-    while( a<radius){
-      var b = -radius
-      while(b<radius){
-        sum =
-        b+=1
-      }
-      a+=1
-    }
+    def apply(x: Int, y: Int): RGBA = data(y * width + x)
+
+    def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
   }
 
 }
